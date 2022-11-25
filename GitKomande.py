6708eed633692cdcf94860ea11913dd835fbcd58
Giappe <predrag.sanader@gmail.com>
2022-11-25 20:49:42 +0100

    dodao fajl

---
+++ GitKomande.py
@@ -0,0 +1,9 @@
+git clone (preuzimanje sa gita)
+-------------------------
+
+git add .
+git commit -m "poruka"
+git push
+-------------------------
+
+git pull (za preuzimanje izmena na gitu, nije kloniranje)
