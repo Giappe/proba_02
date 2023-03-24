a5b93034c74efca113c3bbf8623c758671245406
Giappe <52598725+Giappe@users.noreply.github.com>
2023-03-25 00:52:06 +0100

    Treca nedelja marta

---
+++ 2023_03_24/automobili/automobili/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def proba(request):
+    odgovor = render(request,"proba.html")
+    return odgovor
+
+def naslovnastrana(request):
+    odgovor = render(request,"index.html")
+    return odgovor
+
+def detalji(request):
+    odgovor = HttpResponse("Pozdrav iz strane sa detaljima")
+    return odgovor
+
+
+# pitanja za framework engine-a
+# kako se radi detekcija kolizije
+# kako se pomeraju sprajtovi
+# kako se krece
+
+# baza, parametrizacija, upload, html 
+
